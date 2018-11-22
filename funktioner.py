09fd256d733ede6af5c3dfb5433e92fa7391330f
s175111 <s175111@student.dtu.dk>
2018-11-22 13:13:05 +0100

    init commit

---
+++ funktioner.py
@@ -0,0 +1,18 @@
+def LindIter(System, N):
+    
+    # Insert your code here
+    LindenmayerString = ""
+
+
+    return LindenmayerString
+
+def turtleGraph(LindenmayerString):
+    # Insert your code here
+    turtleCommands = ""
+
+    return turtleCommands
+
+def turtlePlot(turtleCommands):
+    # Insert your code here
+    pass
+
